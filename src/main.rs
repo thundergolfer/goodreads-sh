b2f026be9eef73c1ac193ba2f56773d61860f305
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-06-23 19:21:06 +1000

    add a stub Cli enum to show how subcommands are done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "git", about = "the stupid content tracker")]
+enum Cli {
+    #[structopt(name = "book")]
+    /// TODO: Add a help msg here for Book
+    Book {
+    },
+    #[structopt(name = "author")]
+    /// TODO: Add a help msg here for Author
+    Author {
+    },
+    #[structopt(name = "user")]
+    /// TODO: Add a help msg here for User
+    User {
+        #[structopt(short = "a")]
+        all: bool
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+
+    println!("Actual functionality coming soon!");
 }
