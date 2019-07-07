4fdb5e70677a0512fbc36be9e0f9675552c23d76
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-07-07 18:48:12 +1000

    'update' cmd now begins by pulling the 'currently-reading' shelf

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ use url::form_urlencoded;
 extern crate dirs;
 
 mod goodreads_api {
+    pub const USER_ID: &'static str = "https://www.goodreads.com/api/auth_user";
+    pub const LIST_SHELF: &'static str = "https://www.goodreads.com/review/list?v=2";
     pub const ADD_TO_SHELF: &'static str = "https://www.goodreads.com/shelf/add_to_shelf.xml";
 }
 
@@ -48,6 +50,7 @@ struct GoodReadsConfig {
     developer_secret: String,
     access_token: Option<String>,
     access_token_secret: Option<String>,
+    user_id: Option<i64>
 }
 
 #[derive(Debug)]
@@ -230,7 +233,49 @@ fn run_command(args: &Cli, app_config: &GoodReadsConfig) {
             }
         },
         Cli::Update { .. } => {
-            println!("'update' not yet implemented");
+            let consumer = oauth_client::Token::new(
+                app_config.developer_key.clone(),
+                app_config.developer_secret.clone(),
+            );
+            let access = oauth_client::Token::new(
+                app_config.access_token.as_ref().expect("Access token should never be None here").clone(),
+                app_config.access_token_secret.as_ref().expect("Access token secret should never be None here").clone()
+            );
+            let user_id = app_config.user_id.as_ref().expect("user_id should never be None here").clone();
+            let mut req_param = HashMap::new();
+            let _ = req_param.insert("id".into(), user_id.to_string().into());
+            let _ = req_param.insert("shelf".into(), "currently-reading".into());
+            let _ = req_param.insert("key".into(), app_config.developer_key.clone().into());
+            let (header, body) = oauth_client::authorization_header(
+                "GET",
+                goodreads_api::LIST_SHELF,
+                &consumer,
+                Some(&access),
+                Some(&req_param)
+            );
+            let client = Client::new();
+            let req = client
+                .get(goodreads_api::LIST_SHELF)
+                .header(
+                    reqwest::header::AUTHORIZATION,
+                    header
+                )
+                .header(
+                    reqwest::header::CONTENT_TYPE,
+                    HeaderValue::from_static("application/x-www-form-urlencoded")
+                )
+                .body(body);
+            let resp = req.send();
+            match resp {
+                Ok(mut result) => {
+                    if result.status() == StatusCode::OK {
+                        println!("{}", result.text().unwrap());
+                    } else {
+                        println!("fuck: {}", result.status());
+                    }
+                },
+                Err(err) => println!("fuck: {}", err)
+            }
         },
         Cli::Author { } => println!("'author' not yet implemented."),
         Cli::Authenticate { } => println!("Already authenticated.")
@@ -246,6 +291,7 @@ fn main() {
     let dev_secret: String = cfg.get_str("developer_secret").unwrap();
     let access_token_res: Result<String, _> = cfg.get_str("access_token");
     let access_token_secret_res: Result<String, _> = cfg.get_str("access_token_secret");
+    let user_id_res: Result<i64, _> = cfg.get_int("user_id");
 
     // TODO(Jonathon): Check for both access_token and access_token_secret
     match access_token_res {
@@ -255,6 +301,7 @@ fn main() {
                 developer_key: dev_key,
                 access_token_secret: Some(access_token_secret_res.unwrap()),
                 access_token: Some(access_token),
+                user_id: Some(user_id_res.unwrap()),
             };
             run_command(&args, &app_config);
         }
@@ -263,6 +310,7 @@ fn main() {
                 Cli::Authenticate { } => {
                     let oauth_access_token = get_oauth_token(dev_key, dev_secret);
                     add_access_token_to_config(client_config_path(), &oauth_access_token)
+                    // TODO(Jonathon): Need to also add user_id at this time
                 }
                 _ => println!("OAuth not set up. Please run: goodreads-sh auth")
             }
