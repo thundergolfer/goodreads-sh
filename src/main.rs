827ef02c1976453d13c517507a7326f0c78d44f2
Jonathon Belotti <jonathon@canva.com>
2019-09-04 18:52:04 +1000

    begin moving the code directly assocaited with running user commands into 'actions' module

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ extern crate simple_error;
 
 type BoxResult<T> = Result<T, Box<dyn Error>>;
 
+mod actions;
 mod api_client;
 pub mod models;
 mod ux;
@@ -222,53 +223,7 @@ fn run_command(
     gr_client: &api_client::GoodreadsApiClient,
 ) -> BoxResult<()> {
     match args {
-        Cli::AddToShelf { shelf, title } => {
-            let mut shelf_answer = String::new();
-            let mut title_answer = String::new();
-            let target_shelf = shelf.as_ref().unwrap_or_else(|| {
-                println!("❓: Which shelf would you like to add the book to?");
-                stdin()
-                    .read_line(&mut shelf_answer)
-                    .expect("Failed to read your input");
-                &shelf_answer
-            });
-            let title_query = title.as_ref().unwrap_or_else(|| {
-                println!("🔎 What's the title of the book?");
-                stdin()
-                    .read_line(&mut title_answer)
-                    .expect("Failed to read your input");
-                &title_answer
-            });
-            let res = gr_client
-                .search_books(&title_query, "title")
-                .and_then(|xml| {
-                    models::parse_book_search_results(&xml).map_err(|err| err.to_string())
-                })
-                .and_then(|results| {
-                    println!("📖 Results:");
-                    for (i, result) in results.iter().enumerate() {
-                        println!("{}. {} - {}", i + 1, result.1, result.2);
-                    }
-                    println!("\nWhich one is the one you'd like to add?");
-                    let choice = ux::get_choice(1, results.len() as u32)
-                        .map_err(|_err| "Failed to get user choice")?;
-                    let book_to_update = results
-                        .get((choice as usize) - 1)
-                        .expect("Should never access an invalid index here");
-                    Ok(book_to_update.0)
-                })
-                .and_then(|id| {
-                    gr_client.add_to_shelf(id, target_shelf)?;
-                    Ok(id)
-                });
-            match res {
-                Ok(_) => {
-                    println!("✅ Added to {}!", target_shelf);
-                    Ok(())
-                }
-                Err(err) => bail!("Error: {}", err),
-            }
-        }
+        Cli::AddToShelf { shelf, title } => actions::add_to_shelf(shelf, title, gr_client),
         Cli::New { title } => {
             let mut answer = String::new();
             let title_query = title.as_ref().unwrap_or_else(|| {
